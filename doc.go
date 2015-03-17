a00f78e2e947f624cbac9adf41baefda9c77ac5e
Vincent Batts <vbatts@hashbangbash.com>
2015-03-17 17:34:09 -0400

    doc.go: another req

--- doc.go
+++ doc.go
@@ -3,6 +3,7 @@ What do you expect from a merkle tree API?
 * streaming support
  - building a tree from an io.Reader
  - validating a tree from an io.Reader
+* concurrency safe
 */
 
 package merkle
